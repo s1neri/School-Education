47a620e66e6024e7a112063f567b3b32fab1f981
Serafim Ignatev <sim.ign0@mail.ru>
2025-11-26 01:30:36 +0300

    feat: add landing page - Add third page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import Header from "./Header.js";
 import CarouselCollection from "./Carousel.js";
+import TabsCollection from "./Tabs.js";
 
 new Header()
 new CarouselCollection()
+new TabsCollection()
