13d21726b63bb3ea8ccb9ac461e6f8dfd81513a7
Serafim Ignatev <sim.ign0@mail.ru>
2025-12-02 20:57:45 +0300

    Finally changes -Add six pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import Header from "./Header.js";
 import CarouselCollection from "./Carousel.js";
 import TabsCollection from "./Tabs.js";
+import InputMaskCollection from "./InputMask.js";
 
 new Header()
 new CarouselCollection()
 new TabsCollection()
+new InputMaskCollection()
